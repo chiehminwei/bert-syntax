e9fcdaf32a8bb8ab81e9d85743e7c19225cdb20d
thomwolf <thomwolf@gmail.com>
2019-01-09 09:23:59 +0100

    added gul and mavin tables

---
+++ gen_marvin_tbl_openai_gpt.py
@@ -0,0 +1,39 @@
+import sys
+from collections import *
+
+files=[("base","results/marvin_results_base.txt"),("large","results/marvin_results_large.txt"),("openai_gpt", "results/marvin_results_openai_gpt.txt")]
+
+
+by_model={}
+conditions=set()
+for title,fname in files:
+    lines = open(fname)
+    results=defaultdict(Counter)
+    by_model[title]=results
+    skipped = set()
+    for line in lines:
+        if line.startswith("Better speed"): continue
+        if line.startswith("skipping"):
+            skipped.add(line.split()[1])
+            next(lines)
+            continue
+        res,c1,c2,w1,w2,s = line.split(None, 5)
+        c1 = c1.replace("inanim","anim")
+        conditions.add(c1)
+        results[c1][res]+=1
+
+print("skipped:",skipped)
+
+print("condition & base & large & openai_gpt & count bert & count openai_gpt \\\\")
+for cond in conditions:
+    rb = by_model['base'][cond]
+    rl = by_model['large'][cond]
+    ro = by_model['openai_gpt'][cond]
+    sb = "%.2f" % (rb['True']/(rb['True']+rb['False']))
+    sl = "%.2f" % (rl['True']/(rl['True']+rl['False']))
+    so = "%.2f" % (ro['True']/(ro['True']+ro['False']))
+    print(" & ".join(map(str,[cond, sb, sl, so, sum(rb.values()), sum(ro.values())])),"\\\\")
+
+    
+
+
